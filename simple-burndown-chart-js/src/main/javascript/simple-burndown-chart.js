3f7a44f50e5738ea44545db7a964431452426de4
Andreas Dangel <adangel@users.sourceforge.net>
2013-06-25 20:58:41 +0200

    Refactor code to be reusable - part 1 The simple burndown chart is not executed automatically after including the main javascript file, it must now be executed manually.

--- simple-burndown-chart-js/src/main/javascript/simple-burndown-chart.js
+++ simple-burndown-chart-js/src/main/javascript/simple-burndown-chart.js
@@ -1,91 +1,92 @@
 (function() {
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
 
-d3.xhr("data.json", "application/json", function(error, request) {
-  var parseDate = d3.time.format("%Y-%b-%d").parse;
-  var data = JSON.parse(request.responseText);
+    window.SBD = {
+        render: function() {
+            var margin = {top: 20, right: 20, bottom: 30, left: 50},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom,
+                parseDate = d3.time.format("%Y-%b-%d").parse;
 
-  var startDate = parseDate(data.start);
-  var endDate = parseDate(data.end);
-  var plannedHours = data.plannedHours;
-  
-  // parse time domain
-  var timeDomain = [];
-  for (var i = 0; i < data.timeDomain.length; i++) {
-    timeDomain[i] = parseDate(data.timeDomain[i]);
-  }
-  
-  // parse burndowns
-  for (var i = 0; i < data.burndowns.length; i++) {
-    data.burndowns[i].date = parseDate(data.burndowns[i].date);
-  }
+            d3.xhr("data.json", "application/json", function(error, request) {
+                var data = JSON.parse(request.responseText),
+                    startDate = parseDate(data.start),
+                    endDate = parseDate(data.end),
+                    plannedHours = data.plannedHours;
 
-  var x = d3.time.scale();
+                // parse time domain
+                var timeDomain = [];
+                for (var i = 0; i < data.timeDomain.length; i++) {
+                    timeDomain[i] = parseDate(data.timeDomain[i]);
+                }
 
-  var y = d3.scale.linear()
-      .range([height, 0]);
+                // parse burndowns
+                for (var i = 0; i < data.burndowns.length; i++) {
+                    data.burndowns[i].date = parseDate(data.burndowns[i].date);
+                }
 
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .ticks(function() { return timeDomain; });
+                var x = d3.time.scale(),
+                    y = d3.scale.linear().range([height, 0]);
 
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom")
+                    .ticks(function() { return timeDomain; });
 
-  var line = d3.svg.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.hours); });
+                var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left");
 
-  var svg = d3.select("#chart")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                var line = d3.svg.line()
+                    .x(function(d) { return x(d.date); })
+                    .y(function(d) { return y(d.hours); });
 
-
-  // calculate the output range for the x axis.
-  // input range is timeDomain.
-  var timeOutputRange = [];
-  var currentX = 0;
-  var diff = width / (timeDomain.length - 1);
-  for (var i = 0; i < timeDomain.length; i++) {
-      timeOutputRange.push(currentX);
-      currentX += diff;
-  }
-  x.range(timeOutputRange);
-  x.domain(timeDomain);
-  y.domain([0, d3.max(data.burndowns, function(d) { return d.hours; })]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("y", "-1em")
-      .attr("x", 0)
-      .style("text-anchor", "end")
-      .text("Hours");
-
-  // take the first data point and the date of the last point
-  var ideal = [{date: startDate, hours: plannedHours}, {date: endDate, hours: 0}];
-  svg.append("path")
-      .datum(ideal)
-      .attr("class", "ideal")
-      .attr("d", line);
+                var svg = d3.select("#chart")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-  svg.append("path")
-      .datum(data.burndowns)
-      .attr("class", "line")
-      .attr("d", line);
-});
+                // calculate the output range for the x axis.
+                // input range is timeDomain.
+                var timeOutputRange = [];
+                var currentX = 0;
+                var diff = width / (timeDomain.length - 1);
+                for (var i = 0; i < timeDomain.length; i++) {
+                    timeOutputRange.push(currentX);
+                    currentX += diff;
+                }
+                x.range(timeOutputRange);
+                x.domain(timeDomain);
+                y.domain([0, d3.max(data.burndowns, function(d) { return d.hours; })]);
+
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("y", "-1em")
+                    .attr("x", 0)
+                    .style("text-anchor", "end")
+                    .text("Hours");
+
+                // take the first data point and the date of the last point
+                var ideal = [{date: startDate, hours: plannedHours}, {date: endDate, hours: 0}];
+                svg.append("path")
+                    .datum(ideal)
+                    .attr("class", "ideal")
+                    .attr("d", line);
+
+                svg.append("path")
+                    .datum(data.burndowns)
+                    .attr("class", "line")
+                    .attr("d", line);
+            });
+        }
+    };
 })();
 
